a7831ea6d40b2b91c8b5987b248431e85c0d26d0
Anurag Gaddu <140345394+AnuragGaddu@users.noreply.github.com>
2024-11-23 14:07:42 -0500

    Implement race finishing logic

--- Race.java
+++ Race.java
@@ -1,31 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Race {
-
+    // public ArrayList<Runner> runners = new ArrayList<>();
     public static void main(String[] args) {
-        String[] initials = {"AB", "CD", "EF", "GH", "IJ", "KL",
-                             "MN", "OP", "QR", "ST", "UV", "WX"};
+        Team[] teams = createTeams();
+        Team CA = teams[0];
+        Team DA = teams[1];
+        
+        finishRace(teams);
+
+        // System.out.println(CA.getRunners());
+        // System.out.println(DA.getRunners());
+    }
+
+    public static void finishRace(Team teams[]) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> placements = new ArrayList<>();
+        ArrayList<Integer> bibNumbers = new ArrayList<>();
+
+        for (Team team : teams) {
+            bibNumbers.addAll(team.getAllBibNumbers());
+        }
+
+        // System.out.println(bibNumbers);
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        for (int i = 0; i < Team.getTotalRunners(); i++) {
+            
+
+            System.out.print("Who came in " + (i + 1) + " place? (Bib Number) ");
+            int bibNumber = input.nextInt();
+            if (placements.contains(bibNumber)) {
+                System.out.println("That runner has already finished");
+                i--;
+                continue;
+            } else if (!bibNumbers.contains(bibNumber)) {
+                System.out.println("That bib# is not a valid entry.");
+                System.out.println("Valid entries are: " + bibNumbers);
+                i--;
+                continue;
+            }
+            placements.add(bibNumber);
+            bibNumbers.remove(bibNumbers.indexOf(bibNumber)); 
+        }
+        System.out.println(placements);
+    }
+    
+    public static Team[] createTeams() {
+        String[] initials = {"AB", "CD", "EF", "GH", "IJ", "KL"};
+                            //  "MN", "OP", "QR", "ST", "UV", "WX"};
         
         Team CA = new Team("CA", 1);
         Team DA = new Team("DA", 2);
 
-        for (int i = 0; i < initials.length/2; i++) {
+        for (int i = 0; i < initials.length / 2; i++) {
             CA.addRunner(new Runner(initials[i], 1, 1000 + i));
         }
 
-        for (int i = 6; i < initials.length; i++) {
+        for (int i = initials.length / 2; i < initials.length; i++) {
             DA.addRunner(new Runner(initials[i], 2, 2000 + i));
         }
 
-        System.out.println(CA.getRunners());
-        System.out.println(DA.getRunners());
-
-        // CA.addPoints(1);
-        // CA.addPoints(2);
-        // CA.addPoints(3);
-        // CA.addPoints(4);
-        // CA.addPoints(5);
-        // CA.addPoints(6);
-
-        // System.out.println(CA.getPoints());
+        return new Team[]{CA, DA};
     }
 }
